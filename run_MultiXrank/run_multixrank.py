73c5f933f564b3e2c8aead819ad3448c24b42c61
jedrzejkubica <jj.kubica@student.uw.edu.pl>
2025-12-03 15:11:44 +0100

    run_multixrank.py : remove resource logs

--- run_MultiXrank/run_multixrank.py
+++ run_MultiXrank/run_multixrank.py
@@ -1,8 +1,5 @@
 import sys
-import time, resource
 import multixrank
-
-start = time.time() 
 
 PHENOTYPE = sys.argv[1]
 
@@ -11,10 +8,3 @@ multixrank_obj = multixrank.Multixrank(config=f"{PHENOTYPE}/config.yml", wdir=f"
 ranking_df = multixrank_obj.random_walk_rank()
 
 multixrank_obj.write_ranking(ranking_df, path=f"{PHENOTYPE}/output_{PHENOTYPE}")
-
-end = time.time()
-usage = resource.getrusage(resource.RUSAGE_SELF)
-print(f"wall_time={end-start:.6f}s")
-print(f"user_time={usage.ru_utime:.6f}s")
-print(f"sys_time={usage.ru_stime:.6f}s")
-print(f"max_rss_kb={usage.ru_maxrss}")
